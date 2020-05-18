ad2c2e8e1b3ff4a784555cfa446c4c98e38a9a74
Gundeep Singh <gndp@live.com>
2020-05-18 09:13:57 +0530

    Demo

--- hello/views.py
+++ hello/views.py
@@ -1,13 +1,21 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.conf import settings
+from django.core.files.storage import FileSystemStorage
 
 from .models import Greeting
+import requests
 
 # Create your views here.
 def index(request):
-    # return HttpResponse('Hello from Python!')
-    return render(request, "index.html")
+    return HttpResponse('Hello from Python!')
+    # return simple_upload(request)
+    # return render(request, "index.html")
 
+def teapot(request):
+    r = requests.get('http://httpbin.org/status/418')
+    print(r.text)
+    return HttpResponse('<pre>' + r.text + '</pre>')
 
 def db(request):
 
